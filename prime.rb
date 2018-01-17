66421cffff63da71758cc2bcaeaae770c77af704
Jonathan Dust <d24dust@hotmail.com>
2018-01-17 15:39:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+def prime?(num)
+  range = (1...num + 1)
+  divisors = []
+  range.each do |i|
+    divisors << i if num % i == 0
+  end
+  return divisors.all? { |i| i == 1 || i == num }
+end
